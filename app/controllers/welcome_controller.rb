9c3d18da9c929597febaf43273cc78f462ac686e
blakealtman123 <blakealtman123@gmail.com>
2015-06-18 14:43:45 +0200

    email works and input job descriptions

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,38 @@
 class WelcomeController < ApplicationController
   def index
+  	@users = User.all
+  	@bus_intel = User.where(job_title: 'Business Intelligence')
+  	@sales = User.where(job_title: 'Sales')
+  	@sales_manager = User.where(job_title: 'Sales Manager')
+  	@CEO = User.where(job_title: 'CEO'
+  	@city_planner = User.where(job_title: "City Planner")
+  	@city_planner_manager = User.where(job_title: "City Planner Manager"
+  	@cs = User.where(job_title: "CS"
+  	@cs_manager	= User.where(job_title: "CS Manager")
+  	@cs_team_leader = User.where(job_title: "CS Team Leader")
+  	@durban_sales = User.where(job_title: "Durban Sales")
+  	@durban_manager = User.where(job_title: "Durban City Manager")
+  	@editorial = User.where(job_title: 'Editorial')
+  	@editorial_manager = User.where(job_title: 'Editorial Manager')
+  	@finance = User.where(job_title: 'Finance')
+  	@goods_administrator = User.where(job_title: 'Goods Administrator')
+  	@goods_manager = User.where(job_title: 'Goods Manager')
+  	@goods_sales = User.where(job_title: 'Goods Sales')
+  	@HR_manager = User.where(job_title: 'HR Manager')
+  	@johannesburg_sales = User.where(job_title: 'Johannesburg Sales')
+  	@logistics = User.where(job_title: "Logistics")
+  	@logistics_manager = User.where(job_title: "Logistics Manager")
+  	@pretoria_sales = User.where(job_title: 'Pretoria Sales')
+  	@pretoria_manager = User.where(job_title: 'Pretoria Sales Manager')
+  	@PRM = User.where(job_title: 'PRM')
+  	@PRM_manager = User.where(job_title: 'PRM Manager')
+  	@QA = User.where(job_title: 'QA')
+  	@risk_manager = User.where(job_title: 'Risk Manager')
+  	@sales_manager = User.where(job_title: 'Sales Manager')
+ 	@strategic_accounts = User.where(job_title: 'Strategic Accounts')
+ 	@telesales = User.where(job_title: 'Telesales')
+ 	@telesales_manager = User.where(job_title: 'Telesales Manager')
+ 	@travel = User.where(job_title: 'Travel')
+
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,20 +1,21 @@
+require 'digest/sha2'
 class UserMailer < ApplicationMailer
 
-	def email
-    	@users = User.all
-    	@receiver
+	default "Message-ID"=>"#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@gmail.com"
+
+	def email(user)
+    	@user = user
+    	
+
     	@url_salesperson_peer ='https://docs.google.com/forms/d/1jZSvb3UFvUuM-BQf2AJJfmsG60Y1LzOkmb8EyXBNYl0/viewform'
 		@url_salesperson_subordinate = 'https://docs.google.com/forms/d/1_lzr-j7bmq7pSSTFsbKizJdWllCD-v_HZ4v5ldDbbIg/viewform'
 		@url_salesperson_supervisor = 'https://docs.google.com/forms/d/16hi-amDALRlL9VVYT51rofjQU7svggwWtSHbkISeZ1E/viewform'
 
-
-		@users.each do |user|
-			@receiver = user
-			mail(to: user.email, 
+		mail(to: @user.email, 
 				subject: 'Performance Appraisal', 
 				template_path: 'user_mailer',
 				template_name: 'welcome_email')
-		end
+
 
   	end
 
