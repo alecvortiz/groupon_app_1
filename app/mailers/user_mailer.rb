b6eb7b515fe9927dfa6dc23fff41bfcab9ad1155
Yining <sophietaoyn@gmail.com>
2015-07-06 11:14:59 +0200

    heroku

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,10 @@
 require 'digest/sha2'
 class UserMailer < ApplicationMailer
-	
+	include SendGrid
+    sendgrid_category :use_subject_lines
+  sendgrid_enable   :ganalytics, :opentrack
+  sendgrid_unique_args :key1 => "value1", :key2 => "value2"
+
 
 	default "Message-ID"=>"#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@gmail.com"
    helper UsersHelper
