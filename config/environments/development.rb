5766c1c73f71418e14fbd26d47e68a3a266c8a6c
blakealtman123 <blakealtman123@gmail.com>
2015-07-07 11:09:31 +0200

    development is local, production is heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,19 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-config.action_mailer.default_url_options = {  :host => "calm-beyond-3762.herokuapp.com" }
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.perform_deliveries = true
-config.action_mailer.smtp_settings = {
-  address: 'smtp.mandrillapp.com',
-  port: 25,
- :authentication => :plain,
- :user_name=>"app38575726@heroku.com",
-  :password=>"IiC3MIEw1BSpcX0qoF_XmQ",
-
- :domain         => 'heroku.com',
-  :enable_starttls_auto => true
-}
+config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
 
 
 end
