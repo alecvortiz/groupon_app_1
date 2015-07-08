ca44a883adf41bde5a68c9448b326afb9fdee17d
blakealtman123 <blakealtman123@gmail.com>
2015-07-08 11:35:20 +0200

    homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,9 +33,11 @@ class UsersController < ApplicationController
 	
 
 	def new
+
 	end
 
 	def create
+		
 		@user = User.create(user_params)
 		@user.password = "password123"
 		@user.password_confirmation = "password123"
