001f94458cd8eced3138c483ff813781697932cd
Alexander Ortiz <alecvortiz@Alexanders-MacBook-Pro-7.local>
2015-07-17 15:13:00 +0200

    added loader

--- app/assets/javascripts/turbolinks_transitions.js
+++ app/assets/javascripts/turbolinks_transitions.js
@@ -1,6 +1,10 @@
 document.addEventListener('page:change', function() {
         document.getElementById('primary-content').className += 'animated fadeIn';
+                document.getElementById('sec-content').className += 'animated fadeIn';
+
 });
 document.addEventListener('page:fetch', function() {
         document.getElementById('primary-content').className += 'animated fadeOut';
+                document.getElementById('primary-content').className += 'animated fadeOut';
+
 });
